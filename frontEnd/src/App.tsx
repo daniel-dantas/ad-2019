f32c239045e8dfe51e33ab25de4d1fe52f1b8aef
daniel-dantas <daniel.dantas.developer@gmail.com>
2020-05-18 17:29:03 -0300

    Created App

---
+++ frontEnd/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './global.css'
+
+const App: React.FC = () => {
+  return (
+    <div id="app">
+      <h1>Projeto Adireto</h1>
+    </div>
+  )
+}
+
+export default App;
